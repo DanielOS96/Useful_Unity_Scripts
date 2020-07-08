7961add42ae385e8ca1b84204355102a80ba7ebc
DanielOS96 <39861177+DanielOS96@users.noreply.github.com>
2020-07-08 10:10:26 +0100

    Create Stopwatch.cs

---
+++ Modular/Stopwatch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// Stopwatch to measure the amount of time elapsed in seconds.
+/// </summary>
+public class Stopwatch : MonoBehaviour
+{
+    [System.Serializable]
+    public class MyIntEvent : UnityEvent<int> { }
+
+    public UnityEvent onStopwatchStart;
+    public MyIntEvent onStopwatchStop;
+
+    private bool isActive;
+    private float elapsedTime;
+
+
+    public void StartStopwatch()
+    {
+        isActive = true;
+    }
+
+    public void StopStopwatch()
+    {
+        isActive = false;
+
+        int secondsElapsed = (int)elapsedTime;
+
+
+        //Debug.Log(secondsElapsed);
+
+        onStopwatchStop.Invoke(secondsElapsed);
+    }
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+
+
+}
