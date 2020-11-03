e5475e71935d6c968bc6a5c45702df77bcc6ace2
DanielOS96 <39861177+DanielOS96@users.noreply.github.com>
2020-11-03 15:56:55 -0800

    Create Randomizer.cs

---
+++ Modular/Randomizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Randomizer : MonoBehaviour
+{
+    public List<UnityEvent> events = new List<UnityEvent>();
+
+
+    private int randomListIndex;
+
+
+    public void RandomEvent()
+    {
+        randomListIndex = Random.Range(0, events.Count);
+
+        events[randomListIndex].Invoke();
+    }
+
+
+}
