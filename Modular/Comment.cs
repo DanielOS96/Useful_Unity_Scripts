abe21c001cc4782f582fa18e22042749aa088b63
DanielOS96 <39861177+DanielOS96@users.noreply.github.com>
2020-11-06 11:58:26 -0800

    Removed Unity Warning CS0414

--- Modular/Comment.cs
+++ Modular/Comment.cs
@@ -4,8 +4,10 @@
 /// </summary>
 public class Comment : MonoBehaviour
 {
+#pragma warning disable CS0414
     [TextArea(0,100)]
     [SerializeField]
     private string _notes = "Comment Here."; // Do not place your note/comment here. 
-                                            // Enter your note in the Unity Editor.
+                                             // Enter your note in the Unity Editor.
+#pragma warning restore CS0414
 }
